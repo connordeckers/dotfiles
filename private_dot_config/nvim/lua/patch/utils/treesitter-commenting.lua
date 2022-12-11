9ee2f7fc98dd64d00c99fbfb9b7bcb6bd49f6515
Connor Deckers <dronnoc@gmail.com>
2022-12-11 23:43:37 +1100

    Update .config/nvim/lua/patch/utils/treesitter-commenting.lua

--- private_dot_config/nvim/lua/patch/utils/treesitter-commenting.lua
+++ private_dot_config/nvim/lua/patch/utils/treesitter-commenting.lua
@@ -10,26 +10,38 @@ end
 
 -- TODO Make this work with expressions that can support tree-sitter inherently!
 
+local lookups = {
+  lua = [[
+		 (function_declaration)
+		 (function_definition)
+	]],
+
+  typescript = [[ 
+		 (function)
+		 (arrow_function)
+	]],
+}
+
+lookups.javascript = lookups.typescript
+
 local function FunctionUnderCursor()
   local ts = vim.treesitter
 
   local bufnr = vim.api.nvim_win_get_buf(0)
   local lang = vim.api.nvim_buf_get_option(bufnr, 'filetype')
 
+  local lookup = lookups[lang]
+
+  if lookup == nil then
+    return nil
+  end
+
   local cursor_row, cursor_col = unpack(vim.api.nvim_win_get_cursor(0))
 
   local parser = ts.get_parser(bufnr, lang)
   local syntax_tree = parser:parse()[1]
   local root = syntax_tree:root()
-  local query = ts.parse_query(
-    lang,
-    [[ 
-			([
-			 (function_declaration)
-			 (function_definition)
-			 (function)
-			] @func (#offset! @func 0 0 0 0)) ]]
-  )
+  local query = ts.parse_query(lang, '([ ' .. lookup .. '] @func (#offset! @func 0 0 0 0))')
 
   --
   -- ((variable_declaration) @var (#offset! @var 0 0 0 0))
@@ -67,8 +79,15 @@ local function GetCommentFormat()
   -- Determine whether to use linewise or blockwise commentstring
   local type = '__default'
 
+  local bufnr = vim.api.nvim_win_get_buf(0)
+  local lang = vim.api.nvim_buf_get_option(bufnr, 'filetype')
+
   -- Determine the location where to calculate commentstring from
   local location = utils.get_cursor_location()
+
+  if lang == 'typescript' then
+    return '/** | */'
+  end
 
   return inter.calculate_commentstring { key = type, location = location }
   --end
@@ -82,14 +101,19 @@ function M.insert.before.fn(msg)
     return
   end
 
-  local _, row, col = unpack(method)
+  local row = method[2]
 
   local comment_str = string.format(GetCommentFormat(), msg)
-  local line = string.rep(' ', vim.fn.indent(row + 1)) .. comment_str .. ' '
+  local cursor_index = comment_str:find '|'
+
+  comment_str = comment_str:gsub('|', '')
+
+  local indent = vim.fn.indent(row + 1)
+  local line = string.rep(' ', indent) .. comment_str .. ' '
 
   -- vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { comment_str, string.rep(' ', col) })
   vim.api.nvim_buf_set_lines(0, row, row, false, { line })
-  vim.api.nvim_win_set_cursor(0, { row + 1, col + line:len() })
+  vim.api.nvim_win_set_cursor(0, { row + 1, indent + (cursor_index - 1 or line:len()) })
 
   vim.cmd 'startinsert'
 end
