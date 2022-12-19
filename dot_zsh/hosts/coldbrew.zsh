f9c9586213471f3ae0d5a5d5978017a19b833c36
Connor Deckers <dronnoc@gmail.com>
2022-12-20 09:12:19 +1100

    fix: Don't load tmux in an ssh environment

--- dot_zsh/hosts/coldbrew.zsh
+++ dot_zsh/hosts/coldbrew.zsh
@@ -1,2 +1,13 @@
 alias copy="xclip -selection c"
 alias paste="xclip -selection c -o"
+
+random-wallpaper() {
+	DIR=$HOME/Pictures/wallpapers/
+	SAVED=$DIR/.wallpaper
+	
+	IMG=$(/usr/bin/ls "$DIR" | shuf | head -n 1)
+
+	ln -sf "$IMG" "$SAVED"
+
+	feh --bg-fill "$SAVED"
+}
