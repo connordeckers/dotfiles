6e3a1791865f674319a525d7c40f96094e56b298
Connor Deckers <dronnoc@gmail.com>
2023-05-09 16:57:35 +1000

    Tweaks LSP for icons, trying to find a fix

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -609,14 +609,26 @@ return {
 
         formatting = {
           fields = { 'kind', 'abbr', 'menu' },
-          format = function(entry, vim_item)
-            local kind = require('lspkind').cmp_format { mode = 'symbol_text', maxwidth = 50, ellipsis_char = '...' }(entry, vim_item)
-            local strings = vim.split(kind.kind, '%s', { trimempty = true })
-            kind.kind = ' ' .. (strings[1] or '') .. ' '
-            kind.menu = '    (' .. (strings[2] or '') .. ')'
+          format = require('lspkind').cmp_format {
+            -- mode = 'text', -- show only text annotations
+            mode = 'symbol', -- show only symbol annotations
+            maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
 
-            return kind
-          end,
+            -- The function below will be called before any actual modifications from lspkind
+            -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+            before = function(entry, vim_item)
+              return vim_item
+            end,
+          },
+          -- format = function(entry, vim_item)
+          --   local kind = require('lspkind').cmp_format { mode = 'symbol_text', maxwidth = 50, ellipsis_char = '...' }(entry, vim_item)
+          --   local strings = vim.split(kind.kind, '%s', { trimempty = true })
+          --   kind.kind = ' ' .. (strings[1] or '') .. ' '
+          --   kind.menu = '    (' .. (strings[2] or '') .. ')'
+          --
+          --   return kind
+          -- end,
         },
 
         mapping = cmp.mapping.preset.insert {
